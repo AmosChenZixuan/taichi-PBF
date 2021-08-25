c8aa357ff7bf6b0fdd38e57b6e6904a57aa7d825
AmosChenZixuan <czxvvv@foxmail.com>
2021-08-25 13:42:29 +0800

    Revert "fix pick"
    This reverts commit 3a55a68707f1a5db300b0c4fff77cf37388d1659.

--- src/scene.py
+++ src/scene.py
@@ -21,9 +21,7 @@ class Scene:
     def add_water(self, xy, w, h, wspacing, hspacing, mass=1.):
         sim = self.sim
         mem = sim.mem
-        solver   = sim.solvers[STANDARD][FLUID]
-        startIdx = mem.getNextId()
-
+        solver = sim.solvers[STANDARD][FLUID]
         mem.newMesh()
         x0, y0 = xy
         for i in range(w):
@@ -33,14 +31,11 @@ class Scene:
                 p = Particle(mem.getNextId(), [x,y], mass=mass, phase=FLUID)
                 mem.add(p)
                 solver.add(p)
-        return startIdx, mem.getNextId()-1
 
-    def add_block(self, idx, xy, w, h, mass=1., buoyance=True):
+    def add_block(self, idx, xy, w, h, mass=1.):
         sim = self.sim
         mem = sim.mem
         solver = sim.solvers[SHAPE][idx]
-        startIdx = mem.getNextId()
-
         mem.newMesh()
         x0, y0 = xy
         for i in range(w):
@@ -49,36 +44,8 @@ class Scene:
                 y = y0 + i * 0.25 * sim.grid_size
                 p = Particle(mem.getNextId(), [x,y], mass=mass, phase=SOLID)
                 mem.add(p)
-                solver.add(p)
-                if buoyance:
-                    sim.solvers[STANDARD][FLUID].add(p)
+                solver.add(p); sim.solvers[STANDARD][FLUID].add(p)
         solver.init()
-        return startIdx, mem.getNextId()-1
-
-    def add_rope(self, begin, end, spacing, mass=1., buoyance=True, stiffness=1.):
-        sim = self.sim
-        mem = sim.mem
-        mem.newMesh()
-        startIdx = mem.getNextId()
-
-        x0,y0 = begin
-        x1,y1 = end
-        n = (vec2(*end)-vec2(*begin)).norm()//spacing
-        for i in range(int(n)):
-            pi = mem.getNextId()
-            dx = i * (x1-x0)/n
-            dy = i * (y1-y0)/n
-            p = Particle(pi, [x0+dx,y0+dy], mass=mass , phase=CLOTH)
-            mem.add(p)
-            if buoyance:
-                    sim.solvers[STANDARD][FLUID].add(p)
-            if i > 0:
-                sim.solvers[STANDARD].append(StrechSolver(sim.mem, pi-1, pi, spacing, stiffness))
-        return startIdx, mem.getNextId()-1
-
-####   Abstract Scene
-############################################################
-####   Demo Scene
 
 class FluidScene(Scene):
     def initialize(self):
@@ -125,7 +92,7 @@ class FluidRigidScene(Scene):
         self.add_block(0, (200,100), 10, 10, mass=.5)
         self.add_block(1, (400,100), 10, 10, mass=.9)
 
-class PendulumScene(Scene):
+class TestScene(Scene):
     def initialize(self):
         sim = self.sim
         sim.solvers[STANDARD].append(fluidSolver(sim.mem, sim.grid, sim.grid_size))
@@ -136,22 +103,20 @@ class PendulumScene(Scene):
         sim = self.sim
         mem = sim.mem
         self.add_water((10, 5), 30, 30, 0.4, 0.8, mass=1.)
-        start, end = self.add_rope((300,250), (450, 300), 7, mass=1., buoyance=False)
-        mem.mass[start] = 0.
-        self.add_block(0, (450,300), 5, 5, mass=2.)
-        sim.solvers[STANDARD].append(StrechSolver(sim.mem, end, end+3, 5, 1))
 
-class RopeScene(Scene):
-    def initialize(self):
-        sim = self.sim
-        sim.solvers[STANDARD].append(fluidSolver(sim.mem, sim.grid, sim.grid_size))
-        sim.solvers[STANDARD].append(  gasSolver(sim.mem, sim.grid, sim.grid_size))
+        mem.newMesh()
+        for i in range(30):
+            pi = mem.getNextId()
+            mass = .5 if i > 0 else 0.
+            p = Particle(pi, [300 + 5*i,200], mass=mass , phase=SOLID)
+            mem.add(p)
 
-    def build(self):
-        sim = self.sim
-        self.add_rope((300,950), (301, 350), 7, mass=1., buoyance=True)
+            if i > 0:
+                sim.solvers[STANDARD].append(StrechSolver(sim.mem, pi-1, pi, 5, 1))
+        self.add_block(0, (450,200), 5, 5, mass=.8)
+        sim.solvers[STANDARD].append(StrechSolver(sim.mem, 929, 932, 5, 1))
 
 
 
-GALLARY = [FluidScene, GasScene, PendulumScene, RopeScene, BodyScene, FluidRigidScene]
-DEFAULT_SCENE = RopeScene
+GALLARY = [FluidScene, GasScene, BodyScene, FluidRigidScene]
+DEFAULT_SCENE = TestScene
